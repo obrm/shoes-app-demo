2e81fa4dd64e619bd8ed2cbb52205cb88ebff32d
Ori Baram <obrm770@gmail.com>
2023-10-05 15:11:16 +0300

    example

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,7 +1,7 @@
+import useShoe from './useShoe';
 import useShoeForm from './useShoeForm';
 import { useGlobalAuthContext } from './useGlobalAuthContext';
 import useLoginForm from './useLoginForm';
 import { useGlobalShoeContext } from './useGlobalShoeContext';
-import useShoe from './useShoe';
 
 export { useShoeForm, useLoginForm, useShoe, useGlobalAuthContext, useGlobalShoeContext };
